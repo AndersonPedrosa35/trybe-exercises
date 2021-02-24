03e4e7f24575f6798a6fe513998534688837f51b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-02-24 18:53:32 -0300

    Adicionando requisito 2

--- introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,8 +1,17 @@
 let a = 5;
 let b = 15;
+let c = 25;
 
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log((a + b) % 2) == 0;
+
+if (a > b) {
+    console.log("A venceu");
+}
+else {
+    console.log("B venceu");
+}
+
