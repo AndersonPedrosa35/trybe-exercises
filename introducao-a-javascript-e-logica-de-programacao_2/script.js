12ed06224e5e7bf7f3dfb1290c6481a2a398150e
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-02-25 19:29:43 -0300

    Achando o maior número do array

--- introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -1,5 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-total = 0;
+
 
 // for(var i = 0; i < numbers.length; i ++) {
 //     console.log(numbers[i]);
@@ -9,17 +8,26 @@ total = 0;
 //     total += numbers[i];
 // }
 
+// for (var i = 0; i < numbers.length; i += 1) {
+//         total += numbers[i];
+// }  
+// total = total / numbers.length-1;
+// if (total > 20) {
+//     console.log("Valor maior que 20");
+// }
+// else {
+//     console.log("Valor menos ou igual a 20");
+// }
+
+// Array = Conjunto de variaveis;
+// maior valor do Array;
+// if, for, math.max
+let comparar = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (var i = 0; i < numbers.length; i += 1) {
-        total += numbers[i];
-}  
-total = total / numbers.length-1;
-if (total > 20) {
-    console.log("Valor maior que 20");
-}
-else {
-    console.log("Valor menos ou igual a 20");
-}
-
-
-    
+    if(numbers[i] > comparar) {
+        comparar = numbers[i];
+    }
+        
+}   console.log(comparar);    
 
