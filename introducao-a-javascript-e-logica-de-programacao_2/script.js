5f343e5663524fd6fec7da0e6d9e9a8716d2f752
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-02-25 18:28:44 -0300

    Somando todos os valores no array com for

--- introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -1,5 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+total = 0;
 
-for(var i = 0; i < numbers.length; i ++) {
-    console.log(numbers[i]);
+// for(var i = 0; i < numbers.length; i ++) {
+//     console.log(numbers[i]);
+// }
+
+for (var i = 0; i < numbers.length; i += 1) {
+    total += numbers[i];
 }
+console.log(total);
