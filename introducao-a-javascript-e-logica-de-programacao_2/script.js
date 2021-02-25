c021e7c7b81a08be6850a35961399f304523bec6
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-02-25 19:43:43 -0300

    Achando quantos pares há

--- introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -19,15 +19,24 @@
 //     console.log("Valor menos ou igual a 20");
 // }
 
-// Array = Conjunto de variaveis;
-// maior valor do Array;
-// if, for, math.max
-let comparar = 0;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (var i = 0; i < numbers.length; i += 1) {
-    if(numbers[i] > comparar) {
-        comparar = numbers[i];
-    }
+// let comparar = 0;
+// for (var i = 0; i < numbers.length; i += 1) {
+//     if(numbers[i] > comparar) {
+//         comparar = numbers[i];
+//     }
         
-}   console.log(comparar);    
+// }   console.log(comparar);   
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let par = 0; 
+for (var i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 == 0) {
+        par += 1;
+    }
+}  console.log(par);
+if (par == 0) {
+    console.log("Nenhum par");
+}
+else {
+    console.log("Há pares");
+}
