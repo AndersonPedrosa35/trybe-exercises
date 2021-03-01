378dd4e094ed8f9a197584be4ea289c0e4778569
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-01 15:14:57 -0300

    Fazendo as 3 opções de acessar objetos

--- javascript_dia1/script.js
+++ javascript_dia1/script.js
@@ -1,6 +1,12 @@
 player = {
-  name = 'Marta',
-  lastName = 'Silva',
-  age = 34,
-  medals = { golden: 2, silver: 3 },
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
 }
+
+console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`);
+
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade" );
+
+console.log("A jogadora " + player["name"] + " " + player["lastName"] + " tem " + player["age"] + " anos de idade");
