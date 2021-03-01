e373dc15d099de11a5fb76a436b576d625e6887f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-01 16:16:45 -0300

    Adicionando mais um item ao objeto e imprimindo o objeto no console

---
+++ introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -0,0 +1,12 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
+
+console.log(`Seja bem-vinda ${info.personagem}`);
+console.log(info)
+for (let key in info) {
+  console.log(info[key]);
+}
