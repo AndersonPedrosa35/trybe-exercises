e5afdee035805638e524468e17f2b06fdf6bf33a
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-01 19:51:18 -0300

    Procurando o índice do maior número

--- introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -5,27 +5,42 @@
 //   recorrente: "Sim",
 // };
 
-// let info = {
+// let info2 = {
 //   personagem: "Tio Patinhas",
 //   origem: "Christmas on Bear Mountain",
-//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   nota: "O último MacPatinhas",
 //   recorrente: "Sim",
 // };
 // console.log(`Seja bem-vinda ${info.personagem}`);
-// console.log(info)
-// for (let key in info) {
-//   console.log(info[key]);
-// };
-
+// console.log(info);
+// for (let propries in info && info2) {
+//    if(info[propries] === "Sim" && info2[propries] === "Sim") {
+//      console.log("Ambos recorrentes");
+//    } else {
+//      console.log(info[propries] + " e " + info2[propries]);
+//    }
+//   }
 
 //---------------------------------
 
-function verificaPalindrome(string) {
-  let palindrome = 'arara';
-  if (string == palindrome) {
-    return true;
-  }else {
-    return false;
+// function verificaPalindrome(string) {
+//   let palindrome = 'arara';
+//   if (string == palindrome) {
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
+// console.log(verificaPalindrome("desenvolvedor"))
+let array = [2, 3, 6, 7, 10, 1];
+
+function verificaArray(array) {
+  let indiceMaiorValor = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[indiceMaiorValor] < array[index]) {
+      indiceMaiorValor = index;
+    }
   }
+  return indiceMaiorValor;
 }
-console.log(verificaPalindrome("desenvolvedor"))
+console.log(verificaArray(array))
