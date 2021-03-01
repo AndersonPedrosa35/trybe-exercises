8e12ad173ff0674fc1e3ad4062c70802ff058433
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-01 18:32:04 -0300

    resolvendo exercícios

--- introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,12 +1,29 @@
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: "Sim",
-};
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   recorrente: "Sim",
+// };
 
-console.log(`Seja bem-vinda ${info.personagem}`);
-console.log(info)
-for (let key in info) {
-  console.log(info[key]);
-};
+// let info = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   recorrente: "Sim",
+// };
+// console.log(`Seja bem-vinda ${info.personagem}`);
+// console.log(info)
+// for (let key in info) {
+//   console.log(info[key]);
+// };
+
+
+//---------------------------------
+
+function verificaPalindrome(string) {
+  let palindrome = 'arara';
+  string = palindrome ? true : false;
+  
+  return string;
+}
+console.log(verificaPalindrome("arara"))
