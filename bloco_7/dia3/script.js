b29f8e3e460975e2ddd6dc051f5ccb3d7cca916c
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-29 14:59:09 -0300

    Adicionando exercicio

--- bloco_7/dia3/script.js
+++ bloco_7/dia3/script.js
@@ -82,20 +82,49 @@ const obj3 = {
 // assert.deepStrictEqual(, true, "Retorno inesperao");
 
 // escreva a função addOne aqui
-function addOne (Array) {
-  let newArray = [];
-  for (let index in Array) {
-    newArray.push(Array[index] + 1);
-  }
-  return newArray;
-};
+// function addOne (Array) {
+//   let newArray = [];
+//   for (let index in Array) {
+//     newArray.push(Array[index] + 1);
+//   }
+//   return newArray;
+// };
 
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
+// const myArray = [31, 57, 12, 5];
+// const unchanged = [31, 57, 12, 5];
+// const expected = [32, 58, 13, 6];
+// const output = addOne(myArray);
 
-assert.strictEqual(typeof addOne, 'function');
-assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);
+// assert.strictEqual(typeof addOne, 'function');
+// assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
 
+
+// escreva a função wordLengths aqui
+// function wordLengths (Array) {
+//   let newArray = [];
+//   for (let index in Array) {
+//     newArray.push(Array[index].length);
+//   }
+//   return newArray;
+// }
+
+// const words = ['sun', 'potato', 'roundabout', 'pizza'];
+// const expected = [3, 6, 10, 5];
+
+// assert.strictEqual(typeof wordLengths, 'function');
+// const output = wordLengths(words);
+// assert.deepStrictEqual(output, expected);
+
+
+// escreva a função sumAllNumbers aqui
+function sumAllNumbers (numbers) {
+  return numbers.reduce((total, num) => total + num);
+} 
+
+const numbers = [9, 23, 10, 3, 10];
+const expected = 55;
+const output = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
