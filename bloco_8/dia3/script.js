bd89ee497ca3b4f4c344ec081d3a37fb8f812562
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-04-06 19:34:18 -0300

    Feito exercício 3

--- bloco_8/dia3/script.js
+++ bloco_8/dia3/script.js
@@ -112,38 +112,88 @@ const books = [
 
 // assert.deepStrictEqual(formatedBookNames(), expectedResult);
 
+// const expectedResult = [
+//   {
+//     age: 31,
+//     author: 'Isaac Asimov',
+//   },
+//   {
+//     age: 38,
+//     author: 'H. P. Lovecraft',
+//   },
+//   {
+//     age: 39,
+//     author: 'Stephen King',
+//   },
+//   {
+//     age: 43,
+//     author: 'George R. R. Martin',
+//   },
+//   {
+//     age: 45,
+//     author: 'Frank Herbert',
+//   },
+//   {
+//     age: 62,
+//     author: 'J. R. R. Tolkien',
+//   },
+// ];
+
+// function nameAndAge() {
+//   const book = books.map((element) => {
+//     let age =  element.releaseYear - element.author.birthYear;
+//     return {author : element.author.name, age : age};
+//   })
+//   return Object.values(book).sort((a, b) => a.age - b.age);
+// }
+// assert.deepStrictEqual(nameAndAge(), expectedResult);
+
 const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
+ {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
   },
   {
-    age: 38,
-    author: 'H. P. Lovecraft',
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
   },
   {
-    age: 39,
-    author: 'Stephen King',
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
   },
   {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
   },
 ];
 
-function nameAndAge() {
-  const book = books.map((element) => {
-    let age =  element.releaseYear - element.author.birthYear;
-    return {author : element.author.name, age : age};
-  })
-  return Object.values(book).sort((a, b) => a.age - b.age);
+function fantasyOrScienceFiction() {
+  const book = books.filter((element) => element.genre == 'Fantasia' || element.genre == 'Ficção Científica');
+  return book;
 }
-assert.deepStrictEqual(nameAndAge(), expectedResult);
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
