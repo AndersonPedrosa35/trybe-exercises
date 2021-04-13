d11c75cd245ca19de1486b401a60c571d7f5db32
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-04-13 19:34:03 -0300

    Feito exercicio 2

--- bloco_9/dia2/apiScript.js
+++ bloco_9/dia2/apiScript.js
@@ -16,9 +16,19 @@ const fetchJoke = () => {
     .then(response => response.json()).then((data) => createElement(data.joke));
 };
 
-window.onload = () => fetchJoke();
+// window.onload = () => fetchJoke();
+
+function soma (array, resolve, reject) {
+  let soma = array.reduce((acc, number) => acc + number);
+  if (soma < 800) {
+    return resolve(soma);
+  }
+  return reject(soma);
+}
 
 const promise = new Promise((resolve, reject) => {
 let array = [1, 3, 5, 6, 7, 8, 9, 12, 34, 20];
-
-});
+soma(array, resolve, reject);
+})
+.then((soma) => console.log(`N = ${soma} Sua promisse passou! =)`))
+.catch((soma) => console.log(`N = ${soma} Fracassado! =(`));
