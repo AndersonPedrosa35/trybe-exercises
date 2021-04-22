909374248f8b7ab298a6b0a27a96bc252f77007b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-04-22 19:13:42 -0300

    Feito exercicios 2 e 3

--- bloco_10/dia3/exercicios.test.js
+++ bloco_10/dia3/exercicios.test.js
@@ -1,4 +1,5 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
+const { fn } = require('jest-mock');
 const exercicios = require('./exercicios');
 
 test('Testando números aleatórios', () => {
@@ -6,5 +7,24 @@ test('Testando números aleatórios', () => {
 
   expect(exercicios.randomValue()).toBe(10);
   expect(exercicios.randomValue).toHaveBeenCalled();
+  expect(exercicios.randomValue).toBeCalledTimes(1);
+})
+
+test('Testando divisão', () => {
+  exercicios.randomValue = jest.fn().mockImplementation((a, b) => a / b);
+
+  expect(exercicios.randomValue(2, 2)).toBe(1);
+})
+
+test('Testando multiplicação', () => {
+  exercicios.randomValue = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  expect(exercicios.randomValue(2, 2, 2)).toBe(8);
+  expect(exercicios.randomValue).toBeCalledTimes(1);
+
+  exercicios.randomValue.mockReset();
+  exercicios.randomValue = jest.fn().mockImplementation((a) => a * 2);
+
+  expect(exercicios.randomValue(2)).toBe(4);
   expect(exercicios.randomValue).toBeCalledTimes(1);
 })
