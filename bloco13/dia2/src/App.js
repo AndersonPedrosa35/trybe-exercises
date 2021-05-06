1020017d496cfc998a33ed386a09df2de952aa36
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-06 15:43:47 -0300

    Feito requisitos 1 ao 5

---
+++ bloco13/dia2/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Route exact path='/'>
+      <Home />
+      </Route>
+      <Route path='/About'>
+      <About />
+      </Route>
+      <Route path='/Users'>
+      <Users />
+      </Route>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
