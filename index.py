e967473db2f9ef333685f37ae4f84e24a1d95969
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-30 15:17:42 -0300

    Inserir - index.py - Criação de funções, utilização do input, random para numeros aleatórios e o while para criar um loop enquanto não acerta o numero

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
-mensagem = input('Digite sua mensagem ');
-print(mensagem);
+
+def createMessage():
+    mensagem = input('Digite sua mensagem ');
+    print(mensagem);
+
+import random;
+
+def guessNumber():
+    random_number = random.randint(1, 10)
+    guess = ""
+
+    while guess != random_number:
+        guess = int(input("Qual o seu palpite? "))
+    
+    print("O número sorteado era: ", guess)
+
+guessNumber();
